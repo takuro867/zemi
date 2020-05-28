c1d2a5405cb51bdb7581136b599355823993f756
takuro867 <65551489+takuro867@users.noreply.github.com>
2020-05-28 17:25:11 +0900

    Add files via upload

--- zemi.m
+++ zemi.m
@@ -1,18 +1,15 @@
-D=imread("idata4.bmp")
+D=imread("idata4.bmp");
 imshow(D)
-E=imread("rdata4.bmp")
+E=imread("rdata4.bmp");
 imshow(E)
 %ごま塩ノイズを白に統一して3×3で高輝度の一番低い値にする。
-k=ones(512,2)*255;
-D=[k D k];
-b=ones(2,516)*255;
-D=[b;D;b];
-n=find(~D);
-D(n)=255;
+k=ones(514,514)*255;
+k(2:513,2:513)=D
+n=find(~k);
+k(n)=255;
 fun=@(x) min(x(:));
-D2=nlfilter(D,[3 3],fun);
-D2=D2(3:514,3:514)
-imshow(D)
+D2=nlfilter(k,[3 3],fun);
+D2=D2(2:513,2:513);
 imshow(D2)
 %ここまで
 D3=medfilt2(D2,[5 5],'symmetric')
@@ -21,5 +18,4 @@ E=double(E);
 D3=double(D3);
 MSE=E-D3;
 MSE=MSE.^2/numel(MSE);
-MSE=sum(MSE);
-MSE=sum(MSE)
+MSE=sum(sum(MSE))
